5b1c6f43f8c8f3489b0fd67e5bfefead35b3baa0
gumtow <gumtow@gmail.com>
2021-08-12 22:07:54 -0500

    added redux and default state

---
+++ src/server/defaultState.js
@@ -0,0 +1,23 @@
+export const defaultState = {
+    users:[{
+        id:"U1",
+        name:"Dev"
+    }],
+    groups:[{
+        name: "To Do",
+        id: "G1",
+        owner: "U1"
+    }],
+    tasks:[{
+        name: "Do Tests",
+        id: "T1",
+        group: "G1",
+        owner: "U1",
+        isComplete:false
+    }],
+    comments: [{
+        owenr:"U1",
+        task:"T1",
+        content:"Great work!!!!"
+    }]
+}
