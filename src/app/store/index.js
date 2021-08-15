d446e9a4f88b6695697324b1890ca79c08b84730
gumtow <gumtow@gmail.com>
2021-08-14 22:57:51 -0500

    added ability to update and create tasks

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, combineReducers  } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { defaultState } from "../../server/defaultState";
 import { createLogger } from "redux-logger";
 import createSagaMiddleware from 'redux-saga';
@@ -10,26 +10,39 @@ const sagaMiddleware = createSagaMiddleware();
 export const store = createStore(
     combineReducers({
         tasks(tasks = defaultState.tasks, action) {
-            switch(action.type) {
+            switch (action.type) {
                 case mutations.CREATE_TASK:
                     // console.log(action);
                     return [...tasks, {
-                        id:action.taskID,
-                        name:"New Task",
-                        group:action.groupID,
-                        owner:action.ownerID,
-                        isComplete:false
+                        id: action.taskID,
+                        name: "New Task",
+                        group: action.groupID,
+                        owner: action.ownerID,
+                        isComplete: false
                     }]
+                case mutations.SET_TASK_COMPLETION:
+                    return tasks.map(task => {
+                        return (task.id === action.taskID) ? { ...task, isComplete: action.isComplete } : task;
+                    });
+                case mutations.SET_TASK_NAME:
+                    return tasks.map(task => {
+                        return (task.id === action.taskID) ? { ...task, name: action.name } : task;
+                    });
+                case mutations.SET_TASK_GROUP:
+                    return tasks.map(task => {
+                        return (task.id === action.taskID) ? { ...task, group: action.groupID } : task;
+                    });
+
             }
             return tasks;
         },
-        comments (comments = defaultState.comments) {
+        comments(comments = defaultState.comments) {
             return comments;
         },
-        groups (groups = defaultState.groups) {
+        groups(groups = defaultState.groups) {
             return groups;
         },
-        users (users = defaultState.users) {
+        users(users = defaultState.users) {
             return users;
         }
     }), applyMiddleware(createLogger(), sagaMiddleware)
